766a9872a1c24098a08b9b0b235be323b43de86c
rostislavdmrv <127777371+rostislavdmrv@users.noreply.github.com>
2024-04-17 23:28:31 +0300

    Lab3.5

---
+++ Lab3/Lab3_5/Lab3_5/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab3_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private int randomNumber = new Random().Next(99) + 1;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int enteredNumber = int.Parse(textBox1.Text);
+            if (enteredNumber > randomNumber)
+                MessageBox.Show("It is lower!");
+            else if (enteredNumber < randomNumber)
+                MessageBox.Show("It is grater!");
+            else
+            {
+                MessageBox.Show("You guess it!");
+                Close();
+            }
+        }
+    }
+}
