76c25ac2880e93122891d606c41e1bbceffe083a
rostislavdmrv <127777371+rostislavdmrv@users.noreply.github.com>
2024-04-17 23:29:50 +0300

    Lab4.1

---
+++ Lab4/Lab4.1/Lab4.1/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab4._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String s = (textBox1.Text).ToLower();
+            char[] arr = s.ToCharArray();
+            Array.Reverse(arr);
+            if (s == new String(arr)) MessageBox.Show("Yes. Pilindrom!");
+            else MessageBox.Show("No. The word is not pilindrom!");
+            
+        }
+    }
+
+}
