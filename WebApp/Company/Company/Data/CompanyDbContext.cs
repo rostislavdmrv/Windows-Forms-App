3a943cd895349ab93235199ec709255f3c038477
rostislavdmrv <127777371+rostislavdmrv@users.noreply.github.com>
2024-05-07 22:46:22 +0300

    WebApp

---
+++ WebApp/Company/Company/Data/CompanyDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Company.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Company.Data
+{
+    public class CompanyDbContext : DbContext
+    {
+        public CompanyDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
